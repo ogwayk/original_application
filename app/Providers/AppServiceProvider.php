8eb2ccf6032eb3559a14102f4aaf595bc2043fff
ogawa <uncle.sum1961@gmail.com>
2024-07-15 16:34:04 +0900

    いらなさそうな記述削除

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
-        Paginator::useBootstrapFive();
         \URL::forceScheme('https');
-        $this->app['request']->server->set('HTTPS', 'on');
     }
 }
